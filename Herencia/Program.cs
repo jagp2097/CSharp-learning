272bed13d2d793037ca4be803f39c562cf2a8aa0
jagp2097 <jagp2097@gmail.com>
2021-03-23 23:02:13 -0600

    Herencia y carpeta de curso patrones de diseño

---
+++ Herencia/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace Herencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Caballo Spirit = new Caballo("Spirit");
+
+            Humano Chito = new Humano("Chito");
+
+            Gorila Copito = new Gorila("Copito");
+
+            // Console.WriteLine(Spirit.GetNombre());
+
+            // Console.WriteLine(Chito.GetNombre());
+
+            // Console.WriteLine(Copito.GetNombre());
+
+            // Principio de sustitución (es siempre un...)
+            Mamiferos caballo = new Caballo("Ferrari"); // un Caballo SIEMPRE ES UN Mamifero
+
+            Mamiferos humano = new Humano("Arnulfo"); // un Humano SIEMPRE ES UN Mamifero
+
+
+
+        }
+    }
+
+    class Mamiferos
+    {
+        private string NombreSerVivo;
+
+        public Mamiferos(string NombreSerVivo)
+        {
+            this.NombreSerVivo = NombreSerVivo;
+        }
+
+
+        public void Respirar()
+        {
+            Console.WriteLine("Puedo respirar");
+        }
+
+        public void CuidarCrias()
+        {
+            Console.WriteLine("Puedo criar");
+        }
+
+        public string GetNombre()
+        {
+            return $"El nombre del mamifero es: {this.NombreSerVivo}";  
+        }
+    }
+
+    class Gorila : Mamiferos
+    {
+        // :base() , llamada al constructor de la superclase
+        public Gorila(string NombreGorila) : base(NombreGorila) 
+        {
+
+        }
+
+       public void Trepar()
+        {
+            Console.WriteLine("Puedo trepar");
+        }
+    }
+
+    class Caballo : Mamiferos
+    {
+
+        public Caballo(string NombreCaballo) : base(NombreCaballo)
+        {
+
+        }
+
+        public void Galopar()
+        {
+            Console.WriteLine("Puedo galopar");
+        }
+    }
+
+    class Humano : Mamiferos
+    {
+
+        public Humano(string NombreHumano) : base(NombreHumano)
+        {
+
+        }
+
+        public void Pensar()
+        {
+            Console.WriteLine("Puedo pensar");
+        }
+    }
+
+}
---
+++ curso-design-patterns/solid-srp/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace solid_srp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var report = new CourseReport();
+            report.AddEntry(new CourseReportEntry{ Name = "Patrones de diseño", Students = 1000, Rating = 5 });
+            report.AddEntry(new CourseReportEntry{ Name = "Flutter", Students = 1900, Rating = 4 });
+            report.AddEntry(new CourseReportEntry{ Name = "Android", Students = 100, Rating = 3.8 });
+
+            Console.WriteLine(report.ToString());
+
+            var saver = new FileSaver();
+
+            saver.SaveToFile(@"Reports", "WorkReport.txt", report);
+        }
+    }
+
+    public class CourseReportEntry
+    {
+        public string Name { get; set; }
+        public int Students { get; set; }
+        public double Rating { get; set; }
+    }
+
+    public class CourseReport
+    {
+        //  crear clase que cree guarde el archivo y cree el directoeio
+        private readonly List<CourseReportEntry> _entries;
+
+        public CourseReport()
+        {
+            this._entries = new List<CourseReportEntry>();
+        }
+
+        public void AddEntry(CourseReportEntry entry) => _entries.Add(entry);
+
+        public void RemoveEntryAt(int index) => _entries.RemoveAt(index);
+
+        public override string ToString() => 
+            string.Join(Environment.NewLine, _entries.Select(
+                entry => $"Curso: {entry.Name}, Estudiantes: {entry.Students}, Valoraciones: {entry.Rating}"
+            ));
+    }
+
+    public class FileSaver
+    {
+        public void SaveToFile(string directoryPath, string fileName, CourseReport report)
+        {
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            File.WriteAllText(Path.Combine(directoryPath, fileName), report.ToString());
+        }
+
+    }
+}
