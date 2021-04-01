05213bcc9b405bf0b818a3329746e855e7f24f8d
jagp2097 <jagp2097@gmail.com>
2021-03-31 22:48:08 -0600

    Ejercicio SPR

--- curso-design-patterns/solid-srp/Program.cs
+++ curso-design-patterns/solid-srp/Program.cs
@@ -10,15 +10,15 @@ namespace solid_srp
         static void Main(string[] args)
         {
             var report = new CourseReport();
-            report.AddEntry(new CourseReportEntry{ Name = "Patrones de diseño", Students = 1000, Rating = 5 });
-            report.AddEntry(new CourseReportEntry{ Name = "Flutter", Students = 1900, Rating = 4 });
-            report.AddEntry(new CourseReportEntry{ Name = "Android", Students = 100, Rating = 3.8 });
+
+            report.AddEntry(new CourseReportEntry { Name = "Patrones de diseño", Students= 1000, Rating = 5 });
+            report.AddEntry(new CourseReportEntry { Name = "Flutter", Students = 1900, Rating = 4.5 });
+            report.AddEntry(new CourseReportEntry { Name = "Universidad de Java", Students = 4344, Rating = 4.0 });
 
             Console.WriteLine(report.ToString());
 
-            var saver = new FileSaver();
+            FileSaver.SaveToFile(@"Reports", "WorkReport.txt", report);
 
-            saver.SaveToFile(@"Reports", "WorkReport.txt", report);
         }
     }
 
@@ -31,27 +31,25 @@ namespace solid_srp
 
     public class CourseReport
     {
-        //  crear clase que cree guarde el archivo y cree el directoeio
         private readonly List<CourseReportEntry> _entries;
 
         public CourseReport()
         {
-            this._entries = new List<CourseReportEntry>();
+            _entries = new List<CourseReportEntry>();
         }
 
         public void AddEntry(CourseReportEntry entry) => _entries.Add(entry);
 
         public void RemoveEntryAt(int index) => _entries.RemoveAt(index);
 
-        public override string ToString() => 
-            string.Join(Environment.NewLine, _entries.Select(
-                entry => $"Curso: {entry.Name}, Estudiantes: {entry.Students}, Valoraciones: {entry.Rating}"
-            ));
+        public override string ToString() =>
+            string.Join(Environment.NewLine, _entries.Select(x => $"Curso: {x.Name}, Estudiantes: {x.Students}, Valoración: {x.Rating}"));
+    
     }
 
-    public class FileSaver
+    public static class FileSaver
     {
-        public void SaveToFile(string directoryPath, string fileName, CourseReport report)
+        public static void SaveToFile(string directoryPath, string fileName, CourseReport report)
         {
             if (!Directory.Exists(directoryPath))
             {
@@ -60,6 +58,6 @@ namespace solid_srp
 
             File.WriteAllText(Path.Combine(directoryPath, fileName), report.ToString());
         }
-
     }
+
 }
